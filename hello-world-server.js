7cafa21065c1e9d729af9911a8c129232957ebad
samouhk <58278925+samouhk@users.noreply.github.com>
2021-05-26 17:32:25 +0800

    Add files via upload

---
+++ hello-world-server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+	var today = new Date();
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('The program is called on ' + date + " " + time);
+}).listen(8080);

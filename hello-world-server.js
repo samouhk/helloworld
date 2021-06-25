d14f4e643e2a1d7e03bbadacc3f73b15b8d0c2e1
samouhk <58278925+samouhk@users.noreply.github.com>
2021-06-25 09:53:26 +0800

    Update hello-world-server.js

--- hello-world-server.js
+++ hello-world-server.js
@@ -6,5 +6,5 @@ http.createServer(function (req, res) {
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('The program is called on ' + date + " " + time);
+  res.end('The program is called on ' + date + " " + time + ' by Sam');
 }).listen(8080);
